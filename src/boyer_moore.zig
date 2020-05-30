15a7ec28db274b1fce12ff7cb42a757e03808626
joachimschmidt557 <joachim.schmidt557@outlook.com>
2020-05-30 19:37:35 +0200

    support other types

--- src/boyer_moore.zig
+++ src/boyer_moore.zig
@@ -1,125 +1,137 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const assert = std.debug.assert;
 const testing = std.testing;
 
-/// Returns the maximum length of suffixes of both strings
-fn longestCommonSuffix(a: []const u8, b: []const u8) usize {
-    var i: usize = 0;
-    while (i < a.len and i < b.len) : (i += 1) {
-        if (a[(a.len - 1) - i] != b[(b.len - 1) - i]) {
-            break;
-        }
-    }
-    return i;
-}
+pub fn StringFinder(comptime T: type) type {
+    assert(std.meta.trait.isIndexable(T));
+    const ElemType = std.meta.Elem(T);
+    assert(@typeInfo(ElemType) == .Int);
 
-pub const StringFinder = struct {
-    allocator: ?*Allocator,
-    pattern: []const u8,
-    bad_char: [possible_values]usize,
-    good_suffix: []usize,
+    return struct {
+        allocator: ?*Allocator,
+        pattern: T,
+        bad_char: [possible_values]usize,
+        good_suffix: []usize,
 
-    const possible_values = std.math.maxInt(u8) + 1;
-    const Self = @This();
+        const possible_values = possibleValues(ElemType);
+        const Self = @This();
 
-    /// An empty pattern, requires no allocations
-    pub const empty = Self{
-        .allocator = null,
-        .pattern = "",
-        .bad_char = [_]usize{0} ** possible_values,
-        .good_suffix = &[_]usize{},
-    };
-
-    /// Initializes a StringFinder with a pattern
-    pub fn init(allocator: *Allocator, pattern: []const u8) !Self {
-        if (pattern.len == 0) return Self.empty;
-
-        var self = Self{
-            .allocator = allocator,
-            .pattern = pattern,
-            .bad_char = undefined,
-            .good_suffix = try allocator.alloc(usize, pattern.len),
+        /// An empty pattern, requires no allocations
+        pub const empty = Self{
+            .allocator = null,
+            .pattern = "",
+            .bad_char = [_]usize{0} ** possible_values,
+            .good_suffix = &[_]usize{},
         };
 
-        const last = pattern.len - 1;
-
-        // Initialize bad character rule table
-        for (self.bad_char) |*x| x.* = pattern.len;
-
-        for (pattern) |x, i| {
-            if (i == last) break;
-            self.bad_char[x] = last - i;
+        /// Returns the number of possible values for an integer type
+        fn possibleValues(comptime Type: type) usize {
+            return std.math.maxInt(Type) - std.math.minInt(Type) + 1;
         }
 
-        // Build good suffix rule table
-        var last_prefix = last;
-
-        // First pass
-        {
-            var i = last + 1;
-            while (i > 0) : (i -= 1) {
-                if (std.mem.startsWith(u8, pattern, pattern[i..])) {
-                    last_prefix = i;
-                }
-                self.good_suffix[i - 1] = last_prefix + last - (i - 1);
-            }
-        }
-
-        // Second pass
-        {
+        /// Returns the maximum length of suffixes of both strings
+        fn longestCommonSuffix(a: T, b: T) usize {
             var i: usize = 0;
-            while (i < last) : (i += 1) {
-                const len_suffix = longestCommonSuffix(pattern, pattern[1 .. i + 1]);
-                if (pattern[i - len_suffix] != pattern[last - len_suffix]) {
-                    self.good_suffix[last - len_suffix] = len_suffix + last - i;
+            while (i < a.len and i < b.len) : (i += 1) {
+                if (a[(a.len - 1) - i] != b[(b.len - 1) - i]) {
+                    break;
                 }
             }
+            return i;
         }
 
-        return self;
-    }
+        /// Initializes a StringFinder with a pattern
+        pub fn init(allocator: *Allocator, pattern: T) !Self {
+            if (pattern.len == 0) return Self.empty;
 
-    /// Frees all memory allocated by this string searcher
-    pub fn deinit(self: *Self) void {
-        if (self.allocator) |allocator| {
-            allocator.free(self.good_suffix);
-        }
-    }
+            var self = Self{
+                .allocator = allocator,
+                .pattern = pattern,
+                .bad_char = undefined,
+                .good_suffix = try allocator.alloc(usize, pattern.len),
+            };
 
-    /// Returns the index of the first occurence of the pattern in the
-    /// text. Returns null if the pattern wasn't found
-    pub fn next(self: Self, text: []const u8) ?usize {
-        var i: usize = self.pattern.len;
-        while (i <= text.len) {
-            // Try to match starting from the end of the pattern
-            var j: usize = self.pattern.len;
-            while (j > 0 and text[i - 1] == self.pattern[j - 1]) {
-                i -= 1;
-                j -= 1;
+            const last = pattern.len - 1;
+
+            // Initialize bad character rule table
+            for (self.bad_char) |*x| x.* = pattern.len;
+
+            for (pattern) |x, i| {
+                if (i == last) break;
+                self.bad_char[x] = last - i;
             }
 
-            // If we matched until the beginning of the pattern, we
-            // have a match
-            if (j == 0) {
-                return i;
+            // Build good suffix rule table
+            var last_prefix = last;
+
+            // First pass
+            {
+                var i = last + 1;
+                while (i > 0) : (i -= 1) {
+                    if (std.mem.startsWith(ElemType, pattern, pattern[i..])) {
+                        last_prefix = i;
+                    }
+                    self.good_suffix[i - 1] = last_prefix + last - (i - 1);
+                }
             }
 
-            // Use the bad character table and the good suffix table
-            // to advance our position
-            i += std.math.max(
-                self.bad_char[text[i - 1]],
-                self.good_suffix[j - 1],
-            );
+            // Second pass
+            {
+                var i: usize = 0;
+                while (i < last) : (i += 1) {
+                    const len_suffix = longestCommonSuffix(pattern, pattern[1 .. i + 1]);
+                    if (pattern[i - len_suffix] != pattern[last - len_suffix]) {
+                        self.good_suffix[last - len_suffix] = len_suffix + last - i;
+                    }
+                }
+            }
+
+            return self;
         }
 
-        return null;
-    }
-};
+        /// Frees all memory allocated by this string searcher
+        pub fn deinit(self: *Self) void {
+            if (self.allocator) |allocator| {
+                allocator.free(self.good_suffix);
+            }
+        }
+
+        /// Returns the index of the first occurence of the pattern in the
+        /// text. Returns null if the pattern wasn't found
+        pub fn next(self: Self, text: T) ?usize {
+            var i: usize = self.pattern.len;
+            while (i <= text.len) {
+                // Try to match starting from the end of the pattern
+                var j: usize = self.pattern.len;
+                while (j > 0 and text[i - 1] == self.pattern[j - 1]) {
+                    i -= 1;
+                    j -= 1;
+                }
+
+                // If we matched until the beginning of the pattern, we
+                // have a match
+                if (j == 0) {
+                    return i;
+                }
+
+                // Use the bad character table and the good suffix table
+                // to advance our position
+                i += std.math.max(
+                    self.bad_char[text[i - 1]],
+                    self.good_suffix[j - 1],
+                );
+            }
+
+            return null;
+        }
+    };
+}
 
 test "empty pattern" {
     const allocator = testing.allocator;
 
-    var sf = try StringFinder.init(allocator, "");
+    var sf = try StringFinder([]const u8).init(allocator, "");
     defer sf.deinit();
 
     testing.expectEqual(@as(?usize, 0), sf.next("zig"));
@@ -131,7 +143,7 @@ test "empty pattern" {
 test "pattern with length 1" {
     const allocator = testing.allocator;
 
-    var sf = try StringFinder.init(allocator, "a");
+    var sf = try StringFinder([]const u8).init(allocator, "a");
     defer sf.deinit();
 
     testing.expectEqual(@as(?usize, null), sf.next("zig"));
@@ -143,7 +155,7 @@ test "pattern with length 1" {
 test "test matching" {
     const allocator = testing.allocator;
 
-    var sf = try StringFinder.init(allocator, "zig");
+    var sf = try StringFinder([]const u8).init(allocator, "zig");
     defer sf.deinit();
 
     testing.expectEqual(@as(?usize, 0), sf.next("zig"));
