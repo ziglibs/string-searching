7f355bd3a423f7626720fbaffc8d5c60e40c5b3e
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-12-17 19:20:31 +0100

    Update to latest zig
    zig version 0.11.0-dev.827+8c8f6bfa6

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,8 @@
+const std = @import("std");
+
 pub const bitap = @import("bitap.zig");
 pub const boyer_moore = @import("boyer_moore.zig");
 
-test "" {
-    _ = @import("bitap.zig");
-    _ = @import("boyer_moore.zig");
+test {
+    std.testing.refAllDeclsRecursive(@This());
 }
